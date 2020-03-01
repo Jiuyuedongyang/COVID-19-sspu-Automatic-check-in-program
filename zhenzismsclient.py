794697afb59adf1ee2a2e9182b11e1bc32935697
jiuyuedongyang <lin909958300@yeah.net>
2020-03-01 22:24:58 +0800

    这是第三方短信api模块

---
+++ zhenzismsclient.py
@@ -0,0 +1,39 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+class ZhenziSmsClient(object):
+	def __init__(self, apiUrl, appId, appSecret):
+		self.apiUrl = apiUrl
+		self.appId = appId
+		self.appSecret = appSecret
+
+	def send(self, params):
+		data = params;
+		data['appId'] = self.appId;
+		data['appSecret'] = self.appSecret;
+		requests.packages.urllib3.disable_warnings(InsecureRequestWarning);
+		response = requests.post(self.apiUrl+'/sms/send.do', data=data, verify=False);
+		result = str(response.content,'utf-8');
+		return result;
+
+
+	def balance(self):
+		data = {
+		    'appId': self.appId,
+		    'appSecret': self.appSecret
+		}
+		requests.packages.urllib3.disable_warnings(InsecureRequestWarning);
+		response = requests.post(self.apiUrl+'/account/balance.do', data=data, verify=False);
+		result = str(response.content,'utf-8');
+		return result;
+
+	def findSmsByMessageId(self, messageId):
+		data = {
+		    'appId': self.appId,
+		    'appSecret': self.appSecret,
+		    'messageId': messageId
+		}
+		requests.packages.urllib3.disable_warnings(InsecureRequestWarning);
+		response = requests.post(self.apiUrl+'/smslog/findSmsByMessageId.do', data=data, verify=False);
+		result = str(response.content,'utf-8');
+		return result;
